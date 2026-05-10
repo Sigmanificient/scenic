8fdce99e459049f2503817204079caa617ac3631
Sigmanificient <edhyjox@gmail.com>
2026-05-10 22:24:31 +0200

    add a simple devshell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    applySystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    eachSystem = f:
+      applySystems (system:
+        f {
+          inherit system;
+          pkgs = nixpkgs.legacyPackages.${system};
+        });
+  in {
+    formatter = eachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    devShells = eachSystem ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        env = {
+          CC = pkgs.stdenv.cc;
+          MAKEFLAGS = "-j";
+        };
+
+        packages = with pkgs; [
+          compiledb
+          gcovr
+        ];
+      };
+    });
+  };
+}
