a93fd236b9c97af5b5cf4f9ae997e07a723f2bcc
Sigmanificient <edhyjox@gmail.com>
2026-05-10 23:34:44 +0200

    glibc: use mirror to fetch archive

--- pkgs/glibc/glibc.c
+++ pkgs/glibc/glibc.c
@@ -3,8 +3,8 @@
 const pkg pkgs_glibc = {
     .name        = "glibc",
     .version     = "2.40",
-    .src         = "https://ftp.gnu.org/gnu/glibc/glibc-2.40.tar.xz",
-    .sha256      = "0000000000000000000000000000000000000000000000000000000000000000",
+    .src         = "https://ftpmirror.gnu.org/gnu/glibc/glibc-2.40.tar.xz",
+    .sha256      = "19a890175e9263d748f627993de6f4b1af9cd21e03f080e4bfb3a1fac10205a2",
     .deps        = { .data = NULL, .len = 0 },
     .build_flags = "",
     .build_sys   = BUILD_AUTOTOOLS,
